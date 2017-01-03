3be141d44fa8bc9974d2d4acb8c7765a46435b33
hyojeong Son <web.sonhyoj@gmail.com>
2017-01-03 23:05:24 +0900

    Add files via upload

---
+++ functionBind.js
@@ -0,0 +1,65 @@
+/*
+ * function bind():
+ *
+ * example 1:
+ *
+ * var alice = {
+ *   name: 'alice',
+ *   shout: function(){
+ *     alert(this.name);
+ *   }
+ * }
+ * var boundShout = bind(alice.shout, alice);
+ * boundShout(); // alerts 'alice'
+ * boundShout = bind(alice.shout, {name: 'bob'});
+ * boundShout(); // alerts 'bob'
+ *
+ * example 2:
+ *
+ * var func = function(a, b){ return a + b };
+ * var boundFunc = bind(func, null, 'foo');
+ * var result = boundFunc('bar');
+ * result === 'foobar'; // true
+ *
+*/
+var bind = function(func,context) {
+  var args = Array.prototype.slice.call(arguments,2)
+  return function bound(){
+    var args2 = Array.prototype.slice.call(arguments);
+    return func.apply(context, args.concat(args2))
+  }
+}
+
+/*
+ * Function.prototype.bind:
+ *
+ * example 1:
+ *
+ * var alice = {
+ *   name: 'alice',
+ *   shout: function(){
+ *     alert(this.name);
+ *   }
+ * }
+ *var boundShout = alice.shout.bind(alice);
+ * boundShout(); // alerts 'alice'
+ * boundShout = alice.shout.bind({name: 'bob'});
+ * boundShout(); // alerts 'bob'
+ *
+ * example 2:
+ *
+ * var func = function(a, b){ return a + b };
+ * var boundFunc = func.bind(null, 'foo');
+ * var result = boundFunc('bar');
+ * result === 'foobar'; // true
+ *
+*/
+
+Function.prototype.bind = function(){
+  var fn = this
+  var args = Array.prototype.slice.call(arguments)
+  var obj = args.shift();
+  return function(){
+    return fn.apply(obj,args.concat(Array.prototype.slice.call(arguments)));
+  };
+};
